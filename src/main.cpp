ea12ca22ea46d5ae79b674867eead894d673b7a4
hasith-suran <hasithsuran@gmail.com>
2022-12-26 02:54:44 +0530

    Added initial integration with STL library

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-
-constexpr int compute(int x) {
-    return x * x;
-}
+#include <TC_Vector.h>
 
 int main( int argc, char **argv) {
-    constexpr int res = compute(5);
-    std::cout << "result : " << res << std::endl;
+    techyB::TC_Vector vec;
+    vec.printVec();
     return 0;
 }
