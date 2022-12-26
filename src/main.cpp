0c6364ccade60c49d02095b415251ca78f9984fb
hasithsuran <hasithsuran@gmail.com>
2022-12-26 21:40:03 +0530

    Added bitwise operation demo integration (#3)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <TC_Vector.h>
+#include <TC_BitwiseOp.h>
 
 int main( int argc, char **argv) {
-    techyB::TC_Vector vec;
-    vec.printVec();
+    #ifdef USE_BITWISE_DEMO
+        techyB::TC_BitwiseOp bOp;
+        bOp.performBitLeftShift();
+        bOp.performBitRightShift();
+        bOp.doLogicalBitwiseOperation();
+    #endif
+
+    #ifdef USE_VECTOR_DEMO
+        techyB::TC_Vector vecDemo;
+        vecDemo.printVec();
+    #endif
+
+
+    
     return 0;
 }
