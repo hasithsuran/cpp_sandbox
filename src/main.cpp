e9d6fbe96abe494bc88bc0049d162f3e9e16f2e4
hasith-suran <hasithsuran@gmail.com>
2023-01-01 22:58:23 +0530

    Added core and STL latest demo change integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 #include <TC_Vector.h>
 #include <TC_BitwiseOp.h>
-
-int main( int argc, char **argv) {
-    #ifdef USE_BITWISE_DEMO
-        techyB::TC_BitwiseOp bOp;
-        bOp.performBitLeftShift();
-        bOp.performBitRightShift();
-        bOp.doLogicalBitwiseOperation();
-    #endif
-
-    #ifdef USE_VECTOR_DEMO
-        techyB::TC_Vector vecDemo;
-        vecDemo.printVec();
-    #endif
+#include <TC_Lambda.h>
+#include <TC_Map.h>
+#include <TC_Memory.h>
 
 
-    
+int main(int argc, char **argv)
+{
+#ifdef USE_BITWISE_DEMO
+    techyB::TC_BitwiseOp bOp;
+    bOp.performBitLeftShift();
+    bOp.performBitRightShift();
+    bOp.doLogicalBitwiseOperation();
+#endif
+
+#ifdef USE_VECTOR_DEMO
+    techyB::TC_Vector vecDemo;
+    vecDemo.printVec();
+#endif
+
+#ifdef USE_LAMBDA_DEMO
+    techyB::TC_Lambda lambdaDemo;
+    lambdaDemo.basicLambdaDemo();
+    lambdaDemo.basicLambdaValuePassingDemo();
+#endif
+
+#ifdef USE_MAP_DEMO
+    techyB::TC_Map mapDemo;
+    mapDemo.attributeDemo();
+    mapDemo.insertOrAssign();
+    mapDemo.initDemo();
+    mapDemo.creationDemo();
+#endif
+
+#ifdef USE_BASIC_MEMORY_DEMO
+    techyB::TC_Memory memoryDemo;
+    memoryDemo.basicCopyDemo();
+    memoryDemo.advancedMemoryMovement();
+#endif
     return 0;
 }
